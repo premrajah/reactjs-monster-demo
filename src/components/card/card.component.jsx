a2202373985d4b3b8907e35416db50a6bbab4e05
Prem R <prem.rajah@gmail.com>
2020-07-10 22:54:18 +0100

    added style for card-list added email to card

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,8 @@ import './card.styles.css';
 
 export const Card = (props) => (
   <div className='card-container'>
-    <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt='monster' />
-    <h1 className='card'>{props.monster.name}</h1>
+    <img className='card-image' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt='monster' />
+    <h2 className='card'>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
   </div>
 );
