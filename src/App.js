382a629c605b7694f9a006deddcca319efcd8583
Prem R <prem.rajah@gmail.com>
2020-07-10 23:59:50 +0100

    added search box comp and style edited app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   state = {
@@ -25,10 +26,9 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input
-          type='search'
-          placeholder='Search monsters'
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder={'Search monster'}
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
