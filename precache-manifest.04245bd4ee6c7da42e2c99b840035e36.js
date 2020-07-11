3f5ad73d8232a418f0e7576bc215f07488767e9c
Prem R <prem.rajah@gmail.com>
2020-07-11 23:35:23 +0100

    Updates

---
+++ precache-manifest.04245bd4ee6c7da42e2c99b840035e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4ffc6afed53de3760f86e931014f15",
+    "url": "/reactjs-monster-demo/index.html"
+  },
+  {
+    "revision": "37362dd015c5b6701e84",
+    "url": "/reactjs-monster-demo/static/css/main.8e81e22c.chunk.css"
+  },
+  {
+    "revision": "a6e2d25a84c2a65aa977",
+    "url": "/reactjs-monster-demo/static/js/2.51779f6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-monster-demo/static/js/2.51779f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37362dd015c5b6701e84",
+    "url": "/reactjs-monster-demo/static/js/main.d829e822.chunk.js"
+  },
+  {
+    "revision": "a42b280a58b1ef163c8e",
+    "url": "/reactjs-monster-demo/static/js/runtime-main.4866bf29.js"
+  }
+]);
